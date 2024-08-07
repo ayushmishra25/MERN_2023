9f380cae0e308a35eaa23446e00e379d368ae563
ayushmishra25 <ayushmishrarkt2345@gmail.com>
2024-08-07 13:53:57 +0530

    add new files

---
+++ client/src/pages/About.jsx
@@ -0,0 +1,12 @@
+// ./src/pages/Home.jsx
+import React from 'react';
+
+const Home = () => {
+  return (
+    <div>
+      <h1>About Page</h1>
+    </div>
+  );
+};
+
+export default Home;
--- server/server.js
+++ server/server.js
@@ -1,3 +1,4 @@
+require("dotenv").config();
 const express = require("express");
 const app = express();
 const authRoute = require("./router/auth-router");
